c3209dad1a6de61870938db7e6e7cd48ec0d4602
Rohith <98480349+Rozrc25@users.noreply.github.com>
2024-09-13 08:21:43 +0530

    Update api.js

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,6 +1,6 @@
 const API_DOMAIN = "https://gnews.io/api/v4/top-headlines?country=";
 const API_SEARCH_DOMAIN = "https://gnews.io/api/v4/search?q=";
-const API_KEY = 'bc1916c48a38c10e97e880b56497b99d';
+const API_KEY = 'abaa1623fb92be4d9a5d378cf28d3cba';
 export const endpointPath = (country, category) =>
   `${API_DOMAIN}${country}&lang=en&category=${category}&apikey=${API_KEY}`;
 export const endpointSearch = (searchQuery) =>
